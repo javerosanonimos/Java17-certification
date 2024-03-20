1e3647e34c78b2a99eb264ea69cd8efe0030ff07
Aldo Vargas <aldo@oMac.local>
2024-03-19 22:09:01 -0600

    hello world

---
+++ java17/_1variables/Ambito.java
@@ -0,0 +1,16 @@
+package java17._1variables;
+
+
+/**
+ *
+ * @author JaverosAnonimos
+ */
+public class Ambito {
+    int num;//Variable de clase
+    
+    public void metodo(){
+    int num;//Variable local 
+    num=5;//Se asigna la variable local.
+    this.num=1; //Se asgina la varibale de clase.
+    }
+}
