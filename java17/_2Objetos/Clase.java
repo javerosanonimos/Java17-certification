140c98825fe0c625cfa9e6372688b056898b0f63
Javeos Anónimos <javerosanonimos@gmail.com>
2024-09-10 17:49:24 -0600

    New classes

---
+++ java17/_2Objetos/Clase.java
@@ -0,0 +1,10 @@
+package java17._2Objetos;
+
+public class Clase {
+    public Clase(){
+        //Constructor
+    }
+    public Clase(String s){
+        //Constructor
+    }
+}
---
+++ java17/_2Objetos/Objeto.java
@@ -0,0 +1,6 @@
+package java17._2Objetos;
+
+public class Objeto {
+    Clase c=new Clase();//Crear objeto de la clase 'Clase'
+    Object o=new Object();//Crear objeto de la clase 'Clase'
+}
